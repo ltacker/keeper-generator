ebe1a64fb8e8570ce0d970daa29c01a9c170a484
ltacker <lucas.bertrand.22@gmail.com>
2021-01-12 21:45:50 +0100

    Add main

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ltacker/keeper-generator
+
+go 1.15
+
+require google.golang.org/protobuf v1.25.0
---
+++ main.go
@@ -0,0 +1,8 @@
+package keeper_generator
+
+import "google.golang.org/protobuf/types/pluginpb"
+
+func main() {
+	//req := &plugin.CodeGeneratorRequest{}
+	//resp := &plugin.CodeGeneratorResponse{}
+}
